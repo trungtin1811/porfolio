13b54a77fa1ea2e3389cf71e621a9fbc56579c61
trungtin1811 <se.nttin1811@gmail.com>
2022-01-30 13:45:42 +0700

    animation

--- app.js
+++ app.js
@@ -5,7 +5,25 @@ window.onload = function () {
 };
 
 function preload() {
-  $("#preload").style.display = "none";
-  $("#main").style.display = "block";
+  $("#preload").classList.add("mask-disactive");
+  $("#main").classList.add('body-active');
   console.log($("#preloading"));
 }
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 0;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("reveal-active");
+    } else {
+      reveals[i].classList.remove("reveal-active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
